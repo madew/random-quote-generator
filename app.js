6f16af14fbeb80e6f20759eb70edb5b46e85f340
Michal <michal@MacBook-Pro-Michal.local>
2020-08-19 15:46:03 +0200

    refactored render api response

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const quote_mark = document.querySelector('.quote__mark');
-const quote_author = document.querySelector('#author');
+const quote_author = document.querySelector('.quote__author');
 
 const API = 'https://quotes15.p.rapidapi.com/quotes/random/';
 const API_KEY = '3bf398bdb6mshdc090e989c8ff4bp194aa4jsn310893005fe8';
@@ -16,7 +16,7 @@ function getRandomQuote() {
     .then((data) => {
       console.log(data);
       quote_mark.innerText = `"${data.content}"`;
-      quote_author.innerText = data.originator.name;
+      quote_author.innerText = `\u2014 ${data.originator.name}`;
     });
 }
 
