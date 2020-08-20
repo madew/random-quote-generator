0590b57cdfa69461b066b61e1bead7bec5111eb8
Michal <michal@MacBook-Pro-Michal.local>
2020-08-20 12:55:48 +0200

    added color generator for bg and quote"

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+// DOM Selectors
+const container = document.querySelector('.container');
 const quote_mark = document.querySelector('.quote__mark');
 const quote_author = document.querySelector('.quote__author');
 
+// API Call
 const API = 'https://quotes15.p.rapidapi.com/quotes/random/';
 const API_KEY = '3bf398bdb6mshdc090e989c8ff4bp194aa4jsn310893005fe8';
 
@@ -14,10 +17,22 @@ function getRandomQuote() {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
+      // console.log(data);
       quote_mark.innerText = `"${data.content}"`;
       quote_author.innerText = `\u2014 ${data.originator.name}`;
     });
 }
 
 getRandomQuote();
+
+// Generate Colors
+function generateHslaColors(saturation, ligtness) {
+  const randomColor = Math.floor(Math.random() * 360);
+
+  container.style.backgroundColor = `hsl(${randomColor}, ${saturation}, ${ligtness})`;
+  quote_mark.style.backgroundColor = `hsl(${
+    randomColor + 90
+  }, ${saturation}, ${ligtness})`;
+}
+
+generateHslaColors('75%', '50%');
